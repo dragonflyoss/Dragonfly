c806b3ec87aa52ff46d908006ea8fec412676863
lowzj <zj3142063@gmail.com>
2018-06-12 16:22:36 +0800

    style: use CamelCase instead of ALL_CAPS in Go names

--- dfdaemon/constant/code.go
+++ dfdaemon/constant/code.go
@@ -15,13 +15,13 @@
 package constant
 
 var (
-	CODE_EXIT_USER_HOME_NOT_EXIST = 15
-	CODE_EXIT_PATH_NOT_ABS        = 16
-	CODE_EXIT_RATE_LIMIT_INVALID  = 17
-	CODE_EXIT_PORT_INVALID        = 18
-	CODE_EXIT_DFGET_NOT_FOUND     = 19
-	CODE_EXIT_REPO_CREATE_FAIL    = 20
-	CODE_EXIT_DFGET_FAIL          = 21
+	CodeExitUserHomeNotExist = 15
+	CodeExitPathNotAbs       = 16
+	CodeExitRateLimitInvalid = 17
+	CodeExitPortInvalid      = 18
+	CodeExitDfgetNotFound    = 19
+	CodeExitRepoCreateFail   = 20
+	CodeExitDfgetFail        = 21
 
-	CODE_REQ_AUTH = 22
+	CodeReqAuth = 22
 )
