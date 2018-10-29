31dd6a874b81eb0c839ad17c9293d6a534d8c1b2
Allen Sun <allensun.shl@alibaba-inc.com>
2018-10-29 20:43:13 +0800

    docs: make swagger.yml to define SuperNode's API
    Signed-off-by: Allen Sun <allensun.shl@alibaba-inc.com>

---
+++ hack/generate-swagger-models.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Here is details of the swagger binary we use.
+# $ swagger version
+# version: 0.17.1
+
+# Get the absolute path of this file
+DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+
+swagger generate model -f "$DIR/../apis/swagger.yml" -t "$DIR/../apis" -m types
