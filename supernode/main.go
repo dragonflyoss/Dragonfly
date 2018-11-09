3d5823defed52bd58d65cec272b15c3f1e512401
Allen Sun <allensun.shl@alibaba-inc.com>
2018-11-09 15:00:58 +0800

    feature: rewrite supernode server with golang
    Signed-off-by: Allen Sun <allensun.shl@alibaba-inc.com>

---
+++ supernode/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/alibaba/Dragonfly/supernode/config"
+	"github.com/alibaba/Dragonfly/supernode/daemon"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var cfg = &config.Config{}
+
+func main() {
+	var cmdServe = &cobra.Command{
+		Use:          "Dragonfly Supernode",
+		Long:         "",
+		Args:         cobra.NoArgs,
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runSuperNode(cmd)
+		},
+	}
+
+	setupFlags(cmdServe)
+	if err := cmdServe.Execute(); err != nil {
+		logrus.Error(err)
+		os.Exit(1)
+	}
+}
+
+// setupFlags setups flags for command line.
+func setupFlags(cmd *cobra.Command) {
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+	// flagSet := cmd.PersistentFlags()
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	flagSet := cmd.Flags()
+	flagSet.IntVar(&cfg.ListenPort, "port", 65001, "ListenPort is the port supernode server listens on")
+	flagSet.StringVar(&cfg.HomeDir, "home-dir", "", "HomeDir is working directory of supernode")
+	flagSet.StringVar(&cfg.DownloadPath, "download-path", "downloads", "specifies the path where to store downloaded filed from source address")
+	flagSet.IntVar(&cfg.SystemReservedBindwidth, "system-bindwidth", 20, "Network rate reserved for system (unit: MB/s)")
+	flagSet.IntVar(&cfg.MaxBindwidth, "max-bandwidth", 20, "network rate that supernode can use (unit: MB/s)")
+	flagSet.IntVar(&cfg.SchedulerCorePoolSize, "pool-size", 10, "the core pool size of ScheduledExecutorService")
+	flagSet.BoolVar(&cfg.EnableProfiler, "profiler", false, "Set if supernode HTTP server setup profiler")
+	flagSet.BoolVarP(&cfg.Debug, "debug", "D", false, "Switch daemon log level to DEBUG mode")
+	flagSet.IntVar(&cfg.PeerUpLimit, "up-limit", 5, "upload limit for a peer to serve download tasks")
+	flagSet.IntVar(&cfg.PeerDownLimit, "down-limit", 5, "download limit for supernode to serve download tasks")
+
+}
+
+// runSuperNode prepares configs, setups essential details and runs supernode daemon.
+func runSuperNode(cmd *cobra.Command) error {
+	// initialize log.
+	initLog()
+
+	logrus.Info("start to run supernode")
+
+	d, err := daemon.New(cfg)
+	if err != nil {
+		logrus.Errorf("failed to initialize daemon in supernode: %v", err)
+		return err
+	}
+
+	if err := d.Run(); err != nil {
+		logrus.Errorf("failed to run daemon: %v", err)
+		return err
+	}
+	return nil
+}
+
+// initLog initializes log Level and log format of daemon.
+func initLog() {
+	if cfg.Debug {
+		logrus.Infof("start daemon at debug level")
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	formatter := &logrus.TextFormatter{
+		FullTimestamp:   true,
+		TimestampFormat: time.RFC3339Nano,
+	}
+	logrus.SetFormatter(formatter)
+}
---
+++ supernode/server/router.go
@@ -0,0 +1,96 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/alibaba/Dragonfly/apis/types"
+
+	"github.com/gorilla/mux"
+)
+
+// versionMatcher defines to parse version url path.
+const versionMatcher = "/v{version:[0-9.]+}"
+
+func initRoute(s *Server) *mux.Router {
+	r := mux.NewRouter()
+
+	handlers := []*HandlerSpec{
+		// system
+		{Method: http.MethodGet, Path: "/_ping", HandlerFunc: s.ping},
+	}
+
+	// register API
+	for _, h := range handlers {
+		if h != nil {
+			r.Path(versionMatcher + h.Path).Methods(h.Method).Handler(filter(h.HandlerFunc, s))
+			r.Path(h.Path).Methods(h.Method).Handler(filter(h.HandlerFunc, s))
+		}
+	}
+
+	if s.Config.Debug || s.Config.EnableProfiler {
+		profilerSetup(r)
+	}
+	return r
+}
+
+func profilerSetup(mainRouter *mux.Router) {
+	var r = mainRouter.PathPrefix("/debug/").Subrouter()
+	r.HandleFunc("/pprof/", pprof.Index)
+	r.HandleFunc("/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/pprof/profile", pprof.Profile)
+	r.HandleFunc("/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/pprof/trace", pprof.Trace)
+	r.HandleFunc("/pprof/block", pprof.Handler("block").ServeHTTP)
+	r.HandleFunc("/pprof/heap", pprof.Handler("heap").ServeHTTP)
+	r.HandleFunc("/pprof/goroutine", pprof.Handler("goroutine").ServeHTTP)
+	r.HandleFunc("/pprof/threadcreate", pprof.Handler("threadcreate").ServeHTTP)
+}
+
+func filter(handler Handler, s *Server) http.HandlerFunc {
+	pctx := context.Background()
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		ctx, cancel := context.WithCancel(pctx)
+		defer cancel()
+
+		// Start to handle request.
+
+		if err := handler(ctx, w, req); err != nil {
+			// Handle error if request handling fails.
+			HandleErrorResponse(w, err)
+		}
+		return
+	}
+}
+
+// EncodeResponse encodes response in json.
+func EncodeResponse(rw http.ResponseWriter, statusCode int, data interface{}) error {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(statusCode)
+	return json.NewEncoder(rw).Encode(data)
+}
+
+// HandleErrorResponse handles err from daemon side and constructs response for client side.
+func HandleErrorResponse(w http.ResponseWriter, err error) {
+	var (
+		code   int
+		errMsg string
+	)
+
+	// By default, daemon side returns code 500 if error happens.
+	code = http.StatusInternalServerError
+	errMsg = err.Error()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	enc := json.NewEncoder(w)
+	enc.SetEscapeHTML(false)
+
+	resp := types.Error{
+		Message: errMsg,
+	}
+	enc.Encode(resp)
+}
