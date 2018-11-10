f1f8472f0fc32266afcab1b1690058a8d2896caf
Allen Sun <allensun.shl@alibaba-inc.com>
2018-11-10 16:07:02 +0800

    test: add integration test framework for Dragonfly
    Signed-off-by: Allen Sun <allensun.shl@alibaba-inc.com>

---
+++ test/api_ping_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/dragonflyoss/Dragonfly/test/request"
+
+	"github.com/go-check/check"
+)
+
+// APIPingSuite is the test suite for info related API.
+type APIPingSuite struct{}
+
+func init() {
+	check.Suite(&APIPingSuite{})
+}
+
+// SetUpTest does common setup in the beginning of each test.
+func (suite *APIPingSuite) SetUpTest(c *check.C) {
+}
+
+// TestPing tests /info API.
+func (suite *APIPingSuite) TestPing(c *check.C) {
+	resp, err := request.Get("/_ping")
+	c.Assert(err, check.IsNil)
+	defer resp.Body.Close()
+
+	CheckRespStatus(c, resp, 200)
+}
