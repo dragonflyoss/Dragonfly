9989006cb2ea84028633203c2836a6ecd4de9218
lowzj <zj3142063@gmail.com>
2018-12-06 10:51:49 +0800

    refactor: rename package com.alibaba to com.dragonflyoss
    Signed-off-by: lowzj <zj3142063@gmail.com>

---
+++ src/supernode/src/main/java/com/dragonflyoss/dragonfly/supernode/common/Constants.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 1999-2017 Alibaba Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dragonflyoss.dragonfly.supernode.common;
+
+/**
+ * @author lowzj
+ */
+public final class Constants {
+
+    public static final int PORT = 8001;
+
+    public static String localIp;
+    public static String SUPER_NODE_CID = null;
+
+    public static final int FAIL_COUNT_LIMIT = 5;
+
+    public static final int DEFAULT_SCHEDULER_CORE_POOL_SIZE = 10;
+
+    //-------------------------------------------------------------------------
+    // dfget
+
+    public static final String DFGET_PATH = "/usr/local/bin/dfget";
+
+    //-------------------------------------------------------------------------
+    // directories
+
+    public static final String DEFAULT_BASE_HOME = "/home/admin/supernode";
+    public static final String HTTP_SUB_PATH = "/qtdown/";
+    public static final String DOWN_SUB_PATH = "/download/";
+
+    public static String DOWNLOAD_HOME = DEFAULT_BASE_HOME + "/repo" + DOWN_SUB_PATH;
+    public static String UPLOAD_HOME = DEFAULT_BASE_HOME + "/repo" + HTTP_SUB_PATH;
+
+    //-------------------------------------------------------------------------
+
+    public static final int DEFAULT_PIECE_SIZE = 4 * 1024 * 1024;
+    public static final int PIECE_SIZE_LIMIT = 15 * 1024 * 1024;
+    /**
+     * 4 bytes head and 1 byte tail
+     */
+    public static final int PIECE_HEAD_SIZE = 4;
+    /**
+     * can not change
+     */
+    public static final int PIECE_WRAP_SIZE = PIECE_HEAD_SIZE + 1;
+
+    //-------------------------------------------------------------------------
+
+    public static int PEER_UP_LIMIT = 5;
+    public static int PEER_DOWN_LIMIT = 4;
+    public static int ELIMINATION_LIMIT = 5;
+    /**
+     * unit is KB
+     */
+    public static volatile int LINK_LIMIT = 20 * 1024;
+    /**
+     * unit is MB
+     */
+    public static final int DEFAULT_SYSTEM_NEED_RATE = 20;
+    /**
+     * unit is MB
+     */
+    public static final int DEFAULT_TOTAL_LIMIT = 200;
+
+    //-------------------------------------------------------------------------
+
+    private static String getSuperNodeCidPrefix() {
+        return "cdnnode:" + localIp + "~";
+    }
+
+    public static void generateNodeCid() {
+        if (SUPER_NODE_CID == null) {
+            SUPER_NODE_CID = getSuperNodeCidPrefix();
+        }
+    }
+
+    public static String getSuperCid(String taskId) {
+        return SUPER_NODE_CID + taskId;
+    }
+
+    public volatile static boolean debugSwitcher = false;
+
+    public static boolean isDebugEnabled() {
+        return debugSwitcher;
+    }
+}
---
+++ src/supernode/src/main/java/com/dragonflyoss/dragonfly/supernode/common/view/ResultCode.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 1999-2017 Alibaba Group.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.dragonflyoss.dragonfly.supernode.common.view;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResultCode {
+    public static final int SUCCESS = 200;
+
+    public static final int NOT_FOUND = 404;
+
+    public static final int SYSTEM_ERROR = 500;
+    public static final int PARAM_ERROR = 501;
+    public static final int TARGET_NOT_FOUND = 502;
+
+    public static final int PEER_FINISH = 600;
+    public static final int PEER_CONTINUE = 601;
+    public static final int PEER_WAIT = 602;
+    public static final int PEER_LIMIT = 603;
+    public static final int SUPER_FAIL = 604;
+    public static final int UNKNOWN_ERROR = 605;
+    public static final int TASK_CONFLICT = 606;
+    public static final int URL_NOT_REACH = 607;
+    public static final int NEED_AUTH = 608;
+    public static final int WAIT_AUTH = 609;
+
+    private static final Map<Integer, String> descMap = new HashMap<Integer, String>();
+
+    static {
+        descMap.put(SUCCESS, "success");
+        descMap.put(SYSTEM_ERROR, "system error");
+        descMap.put(PEER_FINISH, "peer task end");
+        descMap.put(PEER_CONTINUE, "peer task go on");
+        descMap.put(PEER_WAIT, "peer task wait");
+        descMap.put(SUPER_FAIL, "super node sync source fail");
+        descMap.put(PARAM_ERROR, "param is illegal");
+        descMap.put(TARGET_NOT_FOUND, "target not found");
+        descMap.put(PEER_LIMIT, "peer down limit");
+        descMap.put(UNKNOWN_ERROR, "unknown error");
+        descMap.put(TASK_CONFLICT, "task conflict");
+        descMap.put(URL_NOT_REACH, "url is not reachable");
+        descMap.put(NEED_AUTH, "need auth");
+        descMap.put(WAIT_AUTH, "wait auth");
+    }
+
+    public static String getDesc(int code) {
+        return descMap.get(code);
+    }
+
+}
