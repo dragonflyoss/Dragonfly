095c9d8d27aa9cf8d072013022030e644a9b5eb0
Allen Sun <allensun.shl@alibaba-inc.com>
2019-01-08 00:53:48 +0800

    feature: add gen-doc command to auto gen dfget cli docs
    Signed-off-by: Allen Sun <allensun.shl@alibaba-inc.com>

---
+++ cmd/dfget/app/gen_doc.go
@@ -0,0 +1,51 @@
+package app
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+// GenDocCommand is used to implement 'gen-doc' command.
+type GenDocCommand struct {
+	cmd *cobra.Command
+
+	// path is the destination path of generated markdown documents.
+	path string
+}
+
+func init() {
+	genDocCommand := &GenDocCommand{}
+	genDocCommand.cmd = &cobra.Command{
+		Use:           "gen-doc",
+		Short:         "Generate Document for dfget command line tool with MarkDown format",
+		Args:          cobra.NoArgs,
+		SilenceErrors: true,
+		SilenceUsage:  true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return genDocCommand.runGenDoc(args)
+		},
+	}
+	genDocCommand.addFlags()
+	rootCmd.AddCommand(genDocCommand.cmd)
+}
+
+// addFlags adds flags for specific command.
+func (g *GenDocCommand) addFlags() {
+	flagSet := g.cmd.Flags()
+
+	flagSet.StringVarP(&g.path, "path", "p", "/tmp", "destination path of generated markdown documents")
+}
+
+func (g *GenDocCommand) runGenDoc(args []string) error {
+	// FIXME: make document path configurable
+	if _, err := os.Stat(g.path); err != nil {
+		if os.IsNotExist(err) {
+			return fmt.Errorf("path %s does not exits, please check your gen-doc input flag --path", g.path)
+		}
+		return err
+	}
+	return doc.GenMarkdownTree(g.cmd.Parent(), g.path)
+}
---
+++ docs/cli_reference/dfget_gen-doc.md
@@ -0,0 +1,23 @@
+## dfget gen-doc
+
+Generate Document for dfget command line tool with MarkDown format
+
+### Synopsis
+
+Generate Document for dfget command line tool with MarkDown format
+
+```
+dfget gen-doc [flags]
+```
+
+### Options
+
+```
+  -h, --help          help for gen-doc
+  -p, --path string   destination path of generated markdown documents (default "/tmp")
+```
+
+### SEE ALSO
+
+* [dfget](dfget.md)	 - The dfget is the client of Dragonfly.
+
