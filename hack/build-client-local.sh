0b55d4655e77c065e9c58d06b89519b7d6b9a632
lowzj <zj3142063@gmail.com>
2019-01-21 17:01:08 +0800

    fix: ci passed but code checks and unit-test failed in actual
    Signed-off-by: lowzj <zj3142063@gmail.com>

--- hack/build-client-local.sh
+++ hack/build-client-local.sh
@@ -9,20 +9,20 @@ BIN_DIR="${BUILD_SOURCE_HOME}/bin"
 
 . ./env.sh
 
-build-dfdaemon-local() {	
-    echo "BUILD: dfdaemon in ${BIN_DIR}/${DFDAEMON_BINARY_NAME}" 
-    test -f "${BIN_DIR}/${DFDAEMON_BINARY_NAME}" && rm -f "${BIN_DIR}/${DFDAEMON_BINARY_NAME}"	
-    cd "${BUILD_SOURCE_HOME}/cmd/dfdaemon" || return	
-    go build -o "${BIN_DIR}/${DFDAEMON_BINARY_NAME}"	
+build-dfdaemon-local() {
+    echo "BUILD: dfdaemon in ${BIN_DIR}/${DFDAEMON_BINARY_NAME}"
+    test -f "${BIN_DIR}/${DFDAEMON_BINARY_NAME}" && rm -f "${BIN_DIR}/${DFDAEMON_BINARY_NAME}"
+    cd "${BUILD_SOURCE_HOME}/cmd/dfdaemon" || return
+    go build -o "${BIN_DIR}/${DFDAEMON_BINARY_NAME}" || return
     chmod a+x "${BIN_DIR}/${DFDAEMON_BINARY_NAME}"
 }
 
-build-dfget-local() {	
-    echo "BUILD: dfget in ${BIN_DIR}/${DFGET_BINARY_NAME}"	
-    test -f "${BIN_DIR}/${DFGET_BINARY_NAME}" && rm -f "${BIN_DIR}/${DFGET_BINARY_NAME}"	
+build-dfget-local() {
+    echo "BUILD: dfget in ${BIN_DIR}/${DFGET_BINARY_NAME}"
+    test -f "${BIN_DIR}/${DFGET_BINARY_NAME}" && rm -f "${BIN_DIR}/${DFGET_BINARY_NAME}"
     cd "${BUILD_SOURCE_HOME}/cmd/dfget"	|| return
-    go build -o "${BIN_DIR}/${DFGET_BINARY_NAME}"	
-    chmod a+x "${BIN_DIR}/${DFGET_BINARY_NAME}"	
+    go build -o "${BIN_DIR}/${DFGET_BINARY_NAME}" || return
+    chmod a+x "${BIN_DIR}/${DFGET_BINARY_NAME}"
 }
 
 main() {
@@ -34,8 +34,7 @@ main() {
             build-dfget-local
         ;;
         *)
-            build-dfget-local
-            build-dfdaemon-local
+            build-dfget-local && build-dfdaemon-local
         ;;
     esac
 }
