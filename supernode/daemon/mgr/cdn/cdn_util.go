465db55148357b7d02ad1fc5bc81362d3b601d11
Starnop <starnopg@gmail.com>
2019-05-06 10:19:29 +0800

    feature: implement the cdn manager cdn reader
    Signed-off-by: Starnop <starnop@163.com>

--- supernode/daemon/mgr/cdn/cdn_util.go
+++ supernode/daemon/mgr/cdn/cdn_util.go
@@ -9,3 +9,8 @@ var getCurrentTimeMillisFunc = getCurrentTimeMillis
 func getCurrentTimeMillis() int64 {
 	return time.Now().UnixNano() / time.Millisecond.Nanoseconds()
 }
+
+// getContentLengthByHeader calculates the piece content length by piece header.
+func getContentLengthByHeader(pieceHeader uint32) int32 {
+	return int32(pieceHeader & 0xffffff)
+}
