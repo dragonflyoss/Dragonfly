1ae59f3b511ab759e270918cd70558cd359b99f3
Alan <zg.zhu@daocloud.io>
2019-07-24 09:58:11 +0800

    fix typo "caculate" -> "calculate"
    fix typo "caculate" -> "calculate"
    
    Signed-off-by: alan <zg.zhu@daocloud.io>

--- supernode/daemon/mgr/task/manager_util.go
+++ supernode/daemon/mgr/task/manager_util.go
@@ -69,7 +69,7 @@ func (tm *Manager) addOrUpdateTask(ctx context.Context, req *types.TaskCreateReq
 		task.Headers = req.Headers
 	}
 
-	// caculate piece size and update the PieceSize and PieceTotal
+	// calculate piece size and update the PieceSize and PieceTotal
 	pieceSize := computePieceSize(fileLength)
 	task.PieceSize = pieceSize
 	task.PieceTotal = int32((fileLength + (int64(pieceSize) - 1)) / int64(pieceSize))
