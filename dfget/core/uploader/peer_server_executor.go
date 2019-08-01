2cce166d697835d70a6214b5fe9b56852ef0d0e1
BingGuanqi <guanqi.bing@daocloud.io>
2019-08-01 13:40:08 +0800

    optimize error description
    optimize error description
    
    Signed-off-by: BingGuanqi <guanqi.bing@daocloud.io>

--- dfget/core/uploader/peer_server_executor.go
+++ dfget/core/uploader/peer_server_executor.go
@@ -52,7 +52,7 @@ func StartPeerServerProcess(cfg *config.Config) (port int, err error) {
 	if defaultExecutor != nil {
 		return defaultExecutor.StartPeerServerProcess(cfg)
 	}
-	return 0, fmt.Errorf("executor of peer server isn't be initialized")
+	return 0, fmt.Errorf("executor of peer server hasn't been initialized")
 }
 
 // PeerServerExecutor starts an independent peer server process for uploading downloaded files.
