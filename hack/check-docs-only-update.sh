3d5e8b0ace7b04b68c2533dfb481b0b801c06046
yeya24 <yb532204897@gmail.com>
2019-12-13 08:35:00 -0500

    update
    Signed-off-by: yeya24 <yb532204897@gmail.com>

--- hack/check-docs-only-update.sh
+++ hack/check-docs-only-update.sh
@@ -4,6 +4,14 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# Extract commit range (or single commit)
+COMMIT_RANGE=$(echo "${CIRCLE_COMPARE_URL}" | cut -d/ -f7)
+
+# Fix single commit, unfortunately we don't always get a commit range from Circle CI
+if [[ $COMMIT_RANGE != *"..."* ]]; then
+  COMMIT_RANGE="${COMMIT_RANGE}...${COMMIT_RANGE}"
+fi
+
 if ! git diff --name-only "$COMMIT_RANGE" | grep -qvE '(\.md)||^(docs/)||^(CONTRIBUTORS)|^(LICENSE)'; then
   echo "Only doc files were updated, not running the CI."
   circleci-agent step halt
