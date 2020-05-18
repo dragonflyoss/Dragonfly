5f3fc061f057886e9bbe97bd843bf978b53d4bbd
allen.wq <allen.wq@alipay.com>
2020-05-18 15:42:07 +0800

    fix

--- dfdaemon/seed/seed_test.go
+++ dfdaemon/seed/seed_test.go
@@ -155,8 +155,8 @@ func (suite *SeedTestSuite) TestSeedSyncReadPerformance(c *check.C) {
 
 	wg := &sync.WaitGroup{}
 
-	// try to download in 10 goroutine
-	for i := 0; i < 10; i++ {
+	// try to download in 5 goroutine
+	for i := 0; i < 5; i++ {
 		rangeSize = 99 * 1023
 		wg.Add(1)
 		go func() {
